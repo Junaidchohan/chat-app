35956fad501687f8e917b37439abe3318fb3ae05
Junaidchohane <junaidchohanedu@gmail.com>
2025-05-09 00:00:56 +0500

    Adding

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -21,28 +21,4 @@ class ChatScreen extends StatelessWidget {
 }
 
 
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-
-class ChatScreen extends StatelessWidget {
-  const ChatScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(onPressed: (){
-            FirebaseAuth.instance.signOut();
-          }, icon: Icon(Icons.exit_to_app, color: Theme.of(context).colorScheme.primary,),),
-        ],
-        title: Text("Flutter chat"),
-      ),
-      body: Center(child: Text("Logged in!")),
-    );
-  }
-}
-
-
-
 
